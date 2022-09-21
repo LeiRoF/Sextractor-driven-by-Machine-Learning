5f1ff2a6d6f200cb55651e76a778e74bf795f2e9
Leirof <vince.lrf@gmail.com>
2022-09-21 18:50:18 +0200

    🔎 Added pupil generation

--- pupil.py
+++ pupil.py
@@ -1,5 +1,32 @@
-def create_pupil(N, radius, obstruction_radius=None, arms=None, arms_size=1):
+from numpy import *
+from scipy.ndimage.interpolation import rotate
+
+def create(N, radius, obstruction_radius=None, arms_count=None, arms_size=1, arms_angle=0):
     """Create a N*N matrix containing a picture of a pupil.
     0 mean that the pixel is situated on an opaque part.
     1 mean that the pixel is situated on a transparent part."""
-    pass
+
+    pupil = ones((N, N))
+
+    X = zeros((N,N))
+    Y = zeros((N,N))
+
+    for i in arange(N):
+        X[i,:] = arange(N)
+        Y[:,i] = arange(N)
+
+    # # Create the main pupil
+    pupil *= sqrt((X-N/2)**2 + (Y-N/2)**2) < radius
+
+    # # Create the obstruction
+    if obstruction_radius is not None:
+        pupil *= sqrt((X-N/2)**2 + (Y-N/2)**2) > obstruction_radius
+
+    # # Create the arms
+    if arms_count is not None:
+        angle = arms_angle
+        for i in arange(arms_count):
+            pupil *= abs((X - N/2)*cos(angle) + (Y - N/2)*sin(angle)) > arms_size
+            angle += 2*pi/arms_count
+
+    return pupil
