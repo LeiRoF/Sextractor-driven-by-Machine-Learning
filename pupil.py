92de43c888f0557216b2ba1925c92c16f2079a57
Leirof <vince.lrf@gmail.com>
2022-11-02 09:40:46 +0100

    Sky picture generation

--- pupil.py
+++ pupil.py
@@ -1,6 +1,8 @@
 from numpy import *
 from scipy.ndimage.interpolation import rotate
+from numba import njit, jit
 
+@jit(nopython=True, cache=True, parrallel=True)
 def create(N, radius, obstruction_radius=None, arms_count=None, arms_size=1, arms_angle=0):
     """Create a N*N matrix containing a picture of a pupil.
     0 mean that the pixel is situated on an opaque part.
@@ -8,8 +10,7 @@ def create(N, radius, obstruction_radius=None, arms_count=None, arms_size=1, arm
 
     pupil = ones((N, N))
 
-    X = ones(N).reshape((N, 1)).dot(arange(N).reshape((1, N)))
-    Y = arange(N).reshape((N, 1)).dot(ones(N).reshape((1, N)))
+    X, Y = meshgrid(arange(N),arange(N))
 
     # Create the main pupil
     pupil *= sqrt((X-N/2)**2 + (Y-N/2)**2) < radius
@@ -22,7 +23,7 @@ def create(N, radius, obstruction_radius=None, arms_count=None, arms_size=1, arm
     if arms_count is not None:
         angle = arms_angle
         for i in arange(arms_count):
-            pupil *= abs((X - N/2)*cos(angle) + (Y - N/2)*sin(angle)) > arms_size
+            pupil *= abs(Y - N/2) > arms_size
             pupil = rotate(pupil, 360/arms_count, reshape=False)
 
     return pupil
